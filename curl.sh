8d40f3542d993e8f01a8af46189647b51252a698
wspark <wspark@wspark@rockplace.co.kr>
2019-04-15 00:17:52 +0900

    Auto Commit

--- curl.sh
+++ curl.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #TokenString=mwrlXN0u-NO64kaSdzfNScC3Vtz6jPKNs7yRHEUJGq
-#curl -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer mwrlXN0u-NO64kaSdzfNScC3Vtz6jPKNs7yRHEUJGqQ' https://192.168.99.100:8443/apis/build.openshift.io/v1/namespace/wspark/buildconfigs/simple-app/instantiate  --insecure --data @start-build.json
 curl -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer mwrlXN0u-NO64kaSdzfNScC3Vtz6jPKNs7yRHEUJGqQ' https://192.168.99.100:8443/apis/build.openshift.io/v1/namespaces/wspark/buildconfigs/simple-app/instantiate  --insecure --data @start-build.json
