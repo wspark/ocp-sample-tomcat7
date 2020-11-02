967d6935a409ca88dd40da2b193d8289ac8694fb
3siksfather <3siksfather@gmail.com>
2020-11-02 13:55:00 +0900

    ok

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
 git commit -m 'ok'
-git push --repo https://wspark:qkrwlsA1@github.com/wspark/sample.git
+git push --repo https://wspark:qkrwlsA1@github.com/wspark/ocp-sample-tomcat7.git
